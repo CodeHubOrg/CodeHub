34410303af8c6b325b52bd3bb7161576645c1e1d
Katja Durrani <mail@kdurrani.co.uk>
2014-01-26 18:50:40 +0000

    adding nav for editing profile

--- themes/codehub/elements/header_profile.php
+++ themes/codehub/elements/header_profile.php
@@ -12,5 +12,22 @@
     </div>
     <div class="inner heading">
       <h2><a href="<?php echo View::url('/profile',$profile->getUserID()); ?>">Profile: <?php echo $profile->getAttribute('full_name'); ?></a></h2>
+      <?php  
+    $u = new User();
+    if($u->getUserID() == $profile->getUserID()) {
+      echo '<p><em>Editing your profile:</em></p>';
+      $nc = Page::getByPath('/profile');
+      $bt = BlockType::getByHandle('autonav');
+      $bt->controller->displayPages = 'custom';
+      $bt->controller->displayPagesCID = $nc->getCollectionID();
+      $bt->controller->orderBy = 'display_asc';
+      $bt->controller->displaySubPages = 'all';
+      $bt->controller->displaySubPageLevels = '1';
+      $bt->controller->displaySystemPages = true;
+      $bt->render('view');
+      echo '<p><em>Some content of this page can be edited by clicking on "Edit" in the top left of the page. Don\'t forget to approve the changes!</em></p>';
+    }
+    ?>
     </div>
+    
   </header>
