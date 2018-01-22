e4ec735c807ee5c88eab51ed885d8d74e665dd99
Katja Durrani <mail@kdurrani.co.uk>
2018-01-22 01:45:46 +0000

    new codehub logo

--- themes/codehub/elements/head.php
+++ themes/codehub/elements/head.php
@@ -1,12 +1,12 @@
- <meta name="viewport" content="width=device-width, initial-scale=1.0" />  
+ <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <!--[if (gt IE 8)|(IEMobile)]><!-->
-      <link href="<?php echo $this->getThemePath(); ?>/stylesheets/non_ie.css" rel="stylesheet" type="text/css" />
+      <link href="<?php echo $this->getThemePath();?>/stylesheets/non_ie.css" rel="stylesheet" type="text/css" />
       <!--<![endif]-->
-     
-      <link href="<?php echo $this->getThemePath(); ?>/stylesheets/screen.css" media="screen, projection" rel="stylesheet" type="text/css" />
+
+      <link href="<?php echo $this->getThemePath();?>/stylesheets/screen.css?2018" media="screen, projection" rel="stylesheet" type="text/css" />
 
       <!--[if (lt IE 9)&(!IEMobile)]>
-            <link href="<?php echo $this->getThemePath(); ?>/stylesheets/ie.css" rel="stylesheet" type="text/css" />
+            <link href="<?php echo $this->getThemePath();?>/stylesheets/ie.css" rel="stylesheet" type="text/css" />
             <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
       <![endif]-->
       <link rel="shortcut icon" href="favicon.ico" />
--- themes/codehub/home.php
+++ themes/codehub/home.php
@@ -1,34 +1,36 @@
 <!Doctype html>
 <html>
 <head>
-  <?php Loader::element('header_required'); ?>
-  <?php $this->inc('elements/head.php'); ?>
+<?php Loader::element('header_required');?>
+  <?php $this->inc('elements/head.php');?>
 </head>
 <body class="index">
   <header id="header">
     <div class="inner heading">
-      
-      <?php 
-      $a = new GlobalArea('Site Logo');
-      $a->display();
-      ?>
-            
-      <?php 
-      $a = new GlobalArea('Header nav');
-      $a->display();
-      ?>
-    </div>
-  
+    <h1>Codehub</h1>
+    <a href="/"><img class="codehub-logo" src="/themes/codehub/img/codehub.svg" onerror="this.src='/themes/codehub/img/codehub.png'" alt="CodeHub logo"></a>
+
+<?php
+//$a = new GlobalArea('Site Logo');
+//$a->display();
+?>
+
+<?php
+$a = new GlobalArea('Header nav');
+$a->display();
+?>
+</div>
+
     <!--  end logo + nav -->
 
-   
+
     <div class="inner margins-vert">
-  
-    <?php 
-    $at = new Area('Intro');
-    $at->display($c);  
-    ?>
-  
+
+<?php
+$at = new Area('Intro');
+$at->display($c);
+?>
+
     </div>
   </header>
 
@@ -36,79 +38,89 @@
 
   <div class="about">
       <section class="about section">
-        
+
           <div class="for-you" >
-            <h2><?php $at = new Area('Heading For-you'); $at->display($c); ?></h2>
+            <h2><?php $at = new Area('Heading For-you');
+$at->display($c);
+?></h2>
               <div class="inner margins-vert">
-                 <?php 
-                 $at = new Area('For-you');
-                 $at->display($c);
-                 ?>
+<?php
+$at = new Area('For-you');
+$at->display($c);
+?>
               </div>
           </div>
 
           <div class="learnings" >
-            <h2><?php $at = new Area('Heading Learnings'); $at->display($c); ?></h2>
+            <h2><?php $at = new Area('Heading Learnings');
+$at->display($c);
+?></h2>
             <div class="inner margins-vert" >
-               <?php 
-               $at = new Area('Learnings');
-               $at->display($c);
-              ?>
+<?php
+$at = new Area('Learnings');
+$at->display($c);
+?>
             </div>
           </div>
 
           <div class="meet">
-          <h2><?php $at = new Area('Heading Meeting'); $at->display($c); ?></h2>
+          <h2><?php $at = new Area('Heading Meeting');
+$at->display($c);
+?></h2>
             <div class="inner margins-vert">
-               <?php 
-                 $at = new Area('Meeting');
-                 $at->display($c);
-                ?>
+<?php
+$at = new Area('Meeting');
+$at->display($c);
+?>
             </div>
           </div>
-          
+
       </section>
-  </div>  
+  </div>
 </div><!-- container -->
 
 <div class="container">
     <section class="organisers section">
-       <h2><?php $at = new Area('Heading Organisers'); $at->display($c); ?></h2>
+       <h2><?php $at = new Area('Heading Organisers');
+$at->display($c);
+?></h2>
       <div class="inner">
         <div class="margins-vert">
-           <?php 
-             $at = new Area('Gicela');
-             $at->display($c);
-            ?>
-        </div>
+<?php
+$at = new Area('Gicela');
+$at->display($c);
+?>
+</div>
         <div class="margins-vert">
-           <?php 
-             $at = new Area('Katja');
-             $at->display($c);
-            ?>
+<?php
+$at = new Area('Katja');
+$at->display($c);
+?>
         </div>
       </div><!-- inner -->
-      
-      <h2><?php $at = new Area('Heading This-site'); $at->display($c); ?></h2>
+
+      <h2><?php $at = new Area('Heading This-site');
+$at->display($c);
+?></h2>
           <div class="margins-vert inner">
-              <?php 
-               $at = new Area('This-site');
-               $at->display($c);
-              ?>
-          </div>
-          
+<?php
+$at = new Area('This-site');
+$at->display($c);
+?>
+</div>
+
           <div class="inner">
-            
-             <?php 
-                 $at = new Area('Contact');
-                 $at->display($c);
-             ?>
+
+<?php
+$at = new Area('Contact');
+$at->display($c);
+?>
           </div>
      </section>
   <footer id="footer">
-    <div class="inner">Codehub <?php echo date("Y"); ?></div>
-  </footer> 
+    <div class="inner">Codehub <?php echo date("Y");?></div>
+  </footer>
 </div>
-  <?php Loader::element('footer_required'); ?>
+<?php Loader::element('footer_required');?>
 </body>
 </html>
